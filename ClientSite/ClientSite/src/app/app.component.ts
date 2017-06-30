43ceec70e60a8a17346f70803ca32c35d972e8b0
Michal Nawrocki <v25@vault25.net>
2017-06-30 21:23:31 +0200

    Client SIte - ready to code :)

--- ClientSite/ClientSite/src/app/app.component.ts
+++ ClientSite/ClientSite/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Http } from '@angular/http'
 @Component({
    selector: 'app-root',
    templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
    constructor(private _httpService: Http) { }
