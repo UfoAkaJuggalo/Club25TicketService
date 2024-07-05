22db20c6c3005f931e05764ac5da3ba2f05f7be1
Michal Nawrocki <v25@vault25.net>
2024-07-05 14:36:03 +0200

    old stuff for client site -- will be removed latter

---
+++ ClientSite/ClientSite/src/app/app.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+   constructor(private _httpService: Http) { }
+   apiValues: string[] = [];
+   ngOnInit() {
+      this._httpService.get('/api/values').subscribe(values => {
+         this.apiValues = values.json() as string[];
+      });
+   }
+}
