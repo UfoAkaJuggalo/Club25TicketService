9b310722eed56fc0b55ceb1f055ae89f6abb7a24
Michal Nawrocki <v25@vault25.net>
2024-07-18 16:58:27 +0200

    added infrastructure ef project and ef configurations

---
+++ api/Infrastructure-EF-SQLdB/EF-Configuration/LanguageConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Club25_Domain.Agregates.EventAgregate.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure_EF_SQLdB.EF_Configuration;
+
+public sealed class LanguageConfiguration : IEntityTypeConfiguration<Language>
+{
+	public void Configure(EntityTypeBuilder<Language> builder)
+	{
+		builder.ToTable("Language");
+
+		builder.Property(e => e.Id)
+		       .ValueGeneratedNever()
+		       .HasColumnName("id");
+		builder.Property(e => e.Name)
+		       .HasMaxLength(15)
+		       .HasColumnName("name");
+		builder.Property(e => e.ShortName)
+		       .HasMaxLength(4)
+		       .HasColumnName("shortName");
+	}
+}
---
+++ api/Infrastructure-EF-SQLdB/EF-Configuration/PermissionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Club25_Domain.Agregates.UserAgregate.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure_EF_SQLdB.EF_Configuration;
+
+public sealed class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+{
+	public void Configure(EntityTypeBuilder<Permission> builder)
+	{
+		builder.ToTable("Permission");
+
+		builder.Property(e => e.Id)
+		       .ValueGeneratedNever()
+		       .HasColumnName("id");
+		builder.Property(e => e.Description)
+		       .HasMaxLength(160)
+		       .HasColumnName("description");
+		builder.Property(e => e.Name)
+		       .HasMaxLength(50)
+		       .HasColumnName("name");
+	}
+}
