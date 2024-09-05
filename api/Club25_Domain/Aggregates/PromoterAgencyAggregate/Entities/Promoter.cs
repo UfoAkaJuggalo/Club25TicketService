976649cbb5290e51b869572e8a6af6b795e87cb1
Michal Nawrocki <v25@vault25.net>
2024-09-05 12:31:11 +0200

    added PromoterAgencyAggregate

---
+++ api/Club25_Domain/Aggregates/PromoterAgencyAggregate/Entities/Promoter.cs
@@ -0,0 +1,11 @@
+﻿using Club25_Domain.Aggregates.PromoterAgencyAggregate.ValueObjects;
+using Club25_Domain.Common.Entities;
+
+namespace Club25_Domain.Aggregates.PromoterAgencyAggregate.Entities;
+
+public sealed class Promoter : Person
+{
+	public required PromoterAgencyContactVO Contact { get; init; }
+	public required PromoterAgency PromoterAgency { get; init; }
+	public required int PromoterAgencyId { get; init; }
+}
---
+++ api/Club25_Domain/Aggregates/PromoterAgencyAggregate/PromoterAgency.cs
@@ -0,0 +1,13 @@
+﻿using Club25_Domain.Aggregates.PromoterAgencyAggregate.Entities;
+using Club25_Domain.Aggregates.PromoterAgencyAggregate.ValueObjects;
+using Club25_Domain.Aggregates.TicketAgencyAggregate.Entities;
+using Club25_Domain.Common.Entities;
+
+namespace Club25_Domain.Aggregates.PromoterAgencyAggregate;
+
+public class PromoterAgency : Organization
+{
+	public required PromoterAgencyContactVO Contact { get; init; }
+	public ICollection<Promoter> Promoters { get; init; } = new List<Promoter>();
+	public ICollection<TicketAgent> Accountants { get; init; } = new List<TicketAgent>();
+}
