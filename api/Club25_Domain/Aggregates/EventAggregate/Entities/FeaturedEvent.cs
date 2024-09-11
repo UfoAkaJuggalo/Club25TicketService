ae8ea55ceeb011288469517ee9382886ef627cd4
Michal Nawrocki <v25@vault25.net>
2024-09-11 12:25:24 +0200

    remove unused entities

--- api/Club25_Domain/Aggregates/EventAggregate/Entities/FeaturedEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Club25_Domain.Aggregates.EventAggregate.Entities;
-
-public class FeaturedEvent
-{
-	public int IdEvent { get; init; }
-
-	public virtual Event IdEventNavigation { get; init; } = null!;
-}
