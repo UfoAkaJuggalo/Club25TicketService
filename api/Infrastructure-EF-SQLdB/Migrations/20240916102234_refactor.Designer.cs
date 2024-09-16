4137244117c23e7ccd9aa7527b2375a0e638e3d2
Michal Nawrocki <v25@vault25.net>
2024-09-16 12:26:48 +0200

    add a new migration with refactor

--- api/Infrastructure-EF-SQLdB/Migrations/20240916102234_refactor.Designer.cs
+++ api/Infrastructure-EF-SQLdB/Migrations/20240916102234_refactor.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure_EF_SQLdB.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure_EF_SQLdB.Migrations
 {
     [DbContext(typeof(Club25Context))]
-    partial class Club25ContextModelSnapshot : ModelSnapshot
+    [Migration("20240916102234_refactor")]
+    partial class refactor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
