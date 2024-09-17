1a42207c3270691651a2062f0a1f5aae760fec15
Michal Nawrocki <v25@vault25.net>
2024-09-17 18:43:20 +0200

    migrations update

---
+++ api/Infrastructure-EF-SQLdB/Migrations/Club25Users/20240917163240_initial.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure_EF_SQLdB.Migrations.Club25Users
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Permission",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    description = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permission", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    login = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    surname = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    pass = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    salt = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    configversion = table.Column<decimal>(type: "decimal(8,4)", precision: 8, scale: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroup",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    description = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroup", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionUser",
+                columns: table => new
+                {
+                    PermissionsId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionUser", x => new { x.PermissionsId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_PermissionUser_Permission_PermissionsId",
+                        column: x => x.PermissionsId,
+                        principalTable: "Permission",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PermissionUser_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionUserGroup",
+                columns: table => new
+                {
+                    PermissionsId = table.Column<int>(type: "int", nullable: false),
+                    UserGroupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionUserGroup", x => new { x.PermissionsId, x.UserGroupId });
+                    table.ForeignKey(
+                        name: "FK_PermissionUserGroup_Permission_PermissionsId",
+                        column: x => x.PermissionsId,
+                        principalTable: "Permission",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PermissionUserGroup_UserGroup_UserGroupId",
+                        column: x => x.UserGroupId,
+                        principalTable: "UserGroup",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserUserGroup",
+                columns: table => new
+                {
+                    UserGroupsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUserGroup", x => new { x.UserGroupsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_UserUserGroup_UserGroup_UserGroupsId",
+                        column: x => x.UserGroupsId,
+                        principalTable: "UserGroup",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUserGroup_User_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermissionUser_UserId",
+                table: "PermissionUser",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermissionUserGroup_UserGroupId",
+                table: "PermissionUserGroup",
+                column: "UserGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUserGroup_UsersId",
+                table: "UserUserGroup",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PermissionUser");
+
+            migrationBuilder.DropTable(
+                name: "PermissionUserGroup");
+
+            migrationBuilder.DropTable(
+                name: "UserUserGroup");
+
+            migrationBuilder.DropTable(
+                name: "Permission");
+
+            migrationBuilder.DropTable(
+                name: "UserGroup");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
