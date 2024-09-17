535dc5cb28f493b3a0a368931746564e5ee81a48
Michal Nawrocki <v25@vault25.net>
2024-09-17 18:41:34 +0200

    addef EF configuration for Clients

---
+++ api/Infrastructure-EF-SQLdB/EF-Configuration/Club25ClientsContext/PurchaseConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Club25_Domain.Aggregates.ClientAggregate.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure_EF_SQLdB.EF_Configuration.Club25ClientsContext;
+
+public sealed class PurchaseConfiguration : IEntityTypeConfiguration<Purchase>
+{
+	public void Configure(EntityTypeBuilder<Purchase> builder)
+	{
+		builder.ToTable(nameof(Purchase))
+		       .HasKey(k => k.Id);
+
+		var columnOrder = 0;
+
+		builder.Property(p => p.Id)
+		       .HasColumnName(nameof(Purchase.Id).ToLower())
+		       .IsRequired()
+		       .HasColumnOrder(columnOrder++)
+		       .HasConversion<Guid>()
+		       .ValueGeneratedOnAdd();
+
+		builder.Property(p => p.PurchaseStatus)
+		       .HasColumnName(nameof(Purchase.PurchaseStatus).ToLower())
+		       .HasColumnOrder(columnOrder++)
+		       .HasConversion<string>();
+
+		builder.Property(p => p.PurchaseDate)
+		       .HasColumnName(nameof(Purchase.PurchaseDate).ToLower())
+		       .HasColumnOrder(columnOrder++)
+		       .HasConversion<DateTime>();
+
+		builder.Property(p => p.TicketId)
+		       .HasColumnName(nameof(Purchase.TicketId).ToLower())
+		       .HasColumnOrder(columnOrder)
+		       .HasConversion<Guid>();
+	}
+}
